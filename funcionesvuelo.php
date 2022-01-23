22c6434885c4de6484c485f2aa6cffee50ded81a
ivanflorido99 <82811512+ivanflorido99@users.noreply.github.com>
2022-01-23 15:47:22 +0100

    Add files via upload

---
+++ funcionesvuelo.php
@@ -0,0 +1,57 @@
+<?php
+include 'arrayvuelos.php';
+
+$Vuelo = $_POST["vuelo"];
+
+/*Media de horas voladas de todos los vuelos*/
+$mediavuelo = array_sum($duracion)/count($duracion);
+
+/*Numero total de pasajeros de todos los vuelos*/
+$pasajeros = array_sum($pasajeros);
+
+/*Número de aviones por fabricante*/
+
+
+/*Destino con más conexiones*/
+
+
+/*Destinos con menos conexiones*/
+
+
+
+#Estadística de Ciudades
+
+/*El número totales de ciudades visitadas*/
+$destino = count($destino);
+
+/*Las veces que se ha ido a una determinada ciudad.*/
+
+
+/*Cual es la ciudad más visitada.*/
+
+
+/*Cuales son las ciudades menos visitadas.*/
+
+
+
+#Estadística del Avión
+
+/*Destinos del avión*/
+
+
+/*Fabricante del avión*/
+
+
+/*Minutos totales*/
+$minutosvolando = array_sum($tiempo);
+
+/*Media de horas*/
+$mediahvoladas = (array_sum($tiempo)/60)/count($tiempo);
+
+/*Media de pasajeros*/
+$pasajeros = array_sum($pasajeros)/count($pasajeros);
+
+/*Pasajeros totales*/
+$pasajeros = array_sum($pasajeros);
+
+?>
