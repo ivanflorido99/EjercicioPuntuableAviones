20e1f789108adc32bcfe00bc5c7b14b25038b0a8
ivanflorido99 <82811512+ivanflorido99@users.noreply.github.com>
2022-01-27 20:52:32 +0100

    Add files via upload

--- funcionesvuelo.php
+++ funcionesvuelo.php
@@ -3,31 +3,36 @@ include 'arrayvuelos.php';
 
 $Vuelo = $_POST["vuelo"];
 
-/*Media de horas*/
-$mediavuelo = array_sum($duracion)/count($duracion);
-
-/*Numero total de pasajeros de todos los vuelos*/
-$pasajeros = array_sum($pasajeros);
-
-/*Número de aviones por fabricante*/
-
-
-
-
-
-
-
-
-
-
-
-
 /*Destino con más conexiones*/
+
+function maximasconexiones ($destino){
+    $maximaconexiones=max(array_column($destino, 'Destino'));
+    echo "las ciudades con mas conexion: " . $maximaconexiones . "<br>";
+}
 
 /*Destinos con menos conexiones*/
 
+function minimasconexiones ($destino){
+    $num=array_count_values(array_column($destino, 'Destino'));
+    
+
+    foreach ($num as $num2 => $nume) {
+        $minmoconexione= min(array_count_values($num));
+        if ($nume == $minmoconexione) {
+           
+            echo "ciudades con menos conexion: "."<br>";
+            echo $num2 . "<br>";
+        }
+    }
+}
+
 /*El número totales de ciudades visitadas*/
-$destino = count($destino);
+function destinos ($destino){
+    $array=array_unique(array_column($destino, 'Destino'));
+    $arrayLi=array_intersect_key($destino,$array);
+    echo " Numero total de ciudades visitadas: ".count($arrayLi)."<br>";
+}
+
 
 /*Las veces que se ha ido a una determinada ciudad.*/
 
@@ -40,14 +45,47 @@ function visitas ($destino){
  }
 }
 
-/*Cual es la ciudad más visitada.*/
+/* ciudad más visitada.*/
 
 
-/*Cuales son las ciudades menos visitadas.*/
+function maximovisitas ($destino){
+    $visitasmax=max(array_column($destino, 'Destino'));
+    echo "La ciudad mas visitada es: " . $visitasmax . "<br>";
+}
+
+/*ciudades menos visitadas.*/
+
+function minimovisitas ($destino){
+    $num=array_count_values(array_column($destino, 'Destino'));
+    echo "Las ciudades menos visitadas son:"."<br>";
+    
+    foreach ($num as $numerovisitas => $numerito ) {
+        $menosvisit=min(array_count_values($num));
+        if ($numerito ==  $menosvisit) {
+            echo $numerovisitas . "<br>";
+        }
+    }
+}
 
 
+#Últimos destinos
 
-/*Destinos del avión*/
+function ultimosdestinos ($destino,$id){
+    
+    echo "  Vueling le informa de que sus ultimos destinos son: ";
+   
+    foreach ($destino as $ultimodestino) {
+           
+        $ulti=$ultimodestino["Destino"];
+          
+        $Vuelo=$ultimodestino["Vuelo"];
+           
+        if ($Vuelo == $id) {
+              
+            echo $ulti.", ";
+            }
+    }
+    }
 
 
 /*Fabricante*/
