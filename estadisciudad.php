93d3f4b632facf7cde44b760493e6e88ac1757c1
ivanflorido99 <82811512+ivanflorido99@users.noreply.github.com>
2022-01-27 20:51:43 +0100

    Add files via upload

--- estadisciudad.php
+++ estadisciudad.php
@@ -1,29 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <title>VUELO</title>
+    <title>Estadistica ciudad</title>
     </head>
 <body>
 <?php
 
 include 'funcionesvuelo.php';
 
-#Llamar Funciones
-
-#Número totales de ciudades visitadas
-
 destinos ($destino);
-
-#Veces que se ha ido a una ciudad.
-
-visita ($destino);
-
-#Ciudad mas visitada 
-
+visita ($destino); 
 maximovisitas ($destino);
-
-#Ciudades menos visitada
-
 minimovisitas ($destino);
 
 ?>
