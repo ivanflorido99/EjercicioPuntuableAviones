f9e7c2f9edaf180efefcd738ce6ab46f3e31c2ce
ivanflorido99 <82811512+ivanflorido99@users.noreply.github.com>
2022-01-27 19:54:32 +0100

    Add files via upload

---
+++ estadisaeropuerto.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>VUELO</title>
+    </head>
+<body>
+<?php
+
+include 'funcionesvuelo.php';
+
+#Llamar Funciones
+
+#Media de horas voladas total
+
+mediahoras ($duracion);
+
+#Numero total de pasajeros
+
+totalpasajeros ($pasajeros);
+
+#Numero de aviones por fabricantes
+
+numerofabricante ($fabricante);
+
+#Ciudad con mas conexiones 
+
+maximasconexiones ($destino);
+
+#Ciudad con menos conexiones 
+
+minimasconexiones ($destino);
+
+?>
+</body>
+</html>
